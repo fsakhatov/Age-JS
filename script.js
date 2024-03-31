87f3a3610cb0e72899ccba0b716b9fa9965137ea
farrukhbek26 <FSAKHATOV@GMAIL.COM>
2024-03-31 21:31:29 +0500

    Updated

--- script.js
+++ script.js
@@ -8,23 +8,48 @@ let p1 = document.querySelector('.p1');
 let p2 = document.querySelector('.p2');
 let p3 = document.querySelector('.p3');
 
+let s1 = document.querySelector('.s1');
+let s2 = document.querySelector('.s2');
+let s3 = document.querySelector('.s3');
+
 let count = 0;
 
+let date = new Date();
 btn.addEventListener('click', (e) => {
     if( day.value.length === 0 || day.value > 31){
     p1.innerText = `Must be a valid day`;
     day.style.borderColor = 'red';
     day.previousElementSibling.style.color = 'red'
+    } else{
+        p1.innerText = '';
+        day.style.borderColor = '#716F6F';
+        day.previousElementSibling.style.color = '#716F6F';
+        count++
     }
     if( month.value.length === 0 || month.value > 12){
         p2.innerText = `Must be a valid month`;
         month.style.borderColor = 'red';
         month.previousElementSibling.style.color = 'red'
+    }else{
+        p2.innerText = '';
+        month.style.borderColor = '#716F6F';
+        month.previousElementSibling.style.color = '#716F6F';
+        count++
     }
     if( year.value.length === 0 || year.value > 2024){
         p3.innerText = `Must be in the past`;
         year.style.borderColor = 'red';
         year.previousElementSibling.style.color = 'red'
+    } else{
+        p3.innerText = '';
+        year.style.borderColor = '#716F6F';
+        year.previousElementSibling.style.color = '#716F6F';
+        count++;
+    }
+    if( count === 3){
+        s1.innerText = (date.getFullYear() - year.value)
+        s2.innerText = (date.getMonth()+1) - month.value
+        s3.innerText = day.value
     }
 }
 
